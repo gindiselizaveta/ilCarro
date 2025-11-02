071cb5749431780aa36e836c0ab28d464871b158
elizavetagindis <gindiselizaveta@gmail.com>
2025-11-02 15:32:25 +0200

    properties1

--- src/main/java/utils/PropertiesReader.java
+++ src/main/java/utils/PropertiesReader.java
@@ -13,7 +13,7 @@ public class PropertiesReader {
     public static String getProperty(String fileName, String key) {
         Properties properties = new Properties();
         try (FileInputStream fileInputStream =
-                     new FileInputStream("src/test/resources" + File.separator + fileName)) {
+                     new FileInputStream("src/test/properties" + File.separator + fileName)) {
             properties.load(fileInputStream);
             return properties.getProperty(key);
         } catch (IOException e) {
